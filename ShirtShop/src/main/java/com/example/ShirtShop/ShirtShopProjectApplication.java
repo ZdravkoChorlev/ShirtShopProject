ea7b977546dc05ddacbd84aea195f760d6a945fa
ZdravkoChorlev <softuni7z@gmail.com>
2019-11-17 20:01:24 +0200

    First commit.

---
+++ ShirtShop/src/main/java/com/example/ShirtShop/ShirtShopProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.ShirtShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ShirtShopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShirtShopProjectApplication.class, args);
+	}
+
+}
---
+++ ShirtShop/src/test/java/com/example/ShirtShop/ShirtShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShirtShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirtShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
